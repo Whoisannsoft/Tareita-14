e3d786fd5f22e1caf26f0eeb3fa70f43598189b8
Ana sofia Gonzalez <soft.ann14@gmail.com>
2024-03-15 10:43:55 -0500

    final

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function ObtenerMayoresIguales (arreglos) {
 function contarElementos (arreglos) {
     return arreglos.length;
 }
+const Menores = ObtenerMenores(fibonacci);
+const Mayores = ObtenerMayoresIguales(fibonacci);
+
+const divMenores = document.getElementById("Menores");
+const divMayores = document.getElementById("Mayores");
+
+const ElementosMenores = contarElementos(Menores);
+const ElementosMayores = contarElementos(Mayores);
+
+divMenores.textContent = "Existe {contarElementos(Menores)} numeros que " + ElementosMenores;
+divMayores.textContent = "Existe {contarElementos(Mayores)} numeros que " + ElementosMayores;
